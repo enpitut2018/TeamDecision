7e63eb3d9a2ec66c705eb274f7518f563fae8e7b
asei1997 <net-use@aseinet.com>
2018-07-25 16:17:00 +0900

    t

--- app/controllers/team_maker_controller.rb
+++ app/controllers/team_maker_controller.rb
@@ -34,10 +34,17 @@ class TeamMakerController < ApplicationController
 
   def join
     #ユーザーテーブルにinsert
-    user = User.new(Rid:1, name:"ABC", email:"test@test.com")
-    user.save
-
-    render html: "ユーザを登録しました Your id = #{user.id}"
+    @Pout = "" # brank
+    @Rid = params[:Rid]
+    if @Rid =~ /^[0-9]+$/ then
+      # @Pout += "int"
+      user = User.new(Rid:@Rid, name:"ABC", email:"test@test.com")
+      user.save
+      @Pout += "ユーザを登録しました Your id = #{user.id}, Rid=#{user.Rid}"
+    else
+      @Pout += "F"      
+    end
+    render html: @Pout
   end
 
   def result

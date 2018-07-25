696c262b7db63bf6a2f8df0d71246d3c6690b5e5
Yamagata <kaito.722.nti@gmail.com>
2018-07-25 10:53:16 +0900

    increment user_id

--- db/migrate/20180725005250_create_users.rb
+++ db/migrate/20180725005250_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
+      t.integer :id , auto_increment: true
       t.integer :Rid
       t.string :name
       t.string :email

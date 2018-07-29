00e378de1984edcef1224801839abcff914f89e9
Nozomi Miyamori <13t4072g@vc.ibaraki.ac.jp>
2018-07-30 00:16:23 +0900

    changed

--- lib/make_team_solver.rb
+++ lib/make_team_solver.rb
@@ -1,33 +1,30 @@
-require 'matrix'
-
 class MakeTeamSolver
-  # p: チーム分けの基準となるパラメータ。降順でソートしておく
+  # p: チーム分けの基準となるパラメータ
   # n: チーム数
   def initialize(p, n)
-    @P = p
-    @N = n
-    @W = p.size / n + (p.size.modulo(n) == 0 ? 0 : 1)# チームあたりの最大ユーザ数
+    @p = p
+    @n = n
   end
 
   # w: チームあたりのユーザ数
   # m: ユーザ数
   def solve()
-    return X(@N, @P.size).to_a
-  end
-
-  # チームテーブルを返す
-  # 貪欲法で解く# n: チーム数
-  def X(n, m)
-    if (n == m)
-        return Matrix::I n
-    else
-        x = X(n, m - 1)
-        q = x * Vector[*@P[0..(m-2)]]
-        return Matrix::columns [*(x.column_vectors), Vector::basis(size: n, 
-            index: q.each_with_index.select {|i|
-              t = x.row(i[1])
-              t.inner_product(t) < @W
-            }.min[1])]
+    ut = [nil] * @p.size
+    team = Array.new(@n) {[]}
+    mean = @p.sum / @n.to_f
+    loop do
+      (0...@n).sort_by {|i|
+        (team[i].sum - mean).abs
+      }.reverse.each {|i|
+        return ut if ut.all?
+        u = ut.each_with_index.select {|j|
+          j.include? nil
+        }.min_by {|j|
+          (team[i].sum + @p[j[1]] - mean).abs
+        }
+        ut[u[1]] = i
+        team[i] << @p[u[1]]
+      }
     end
   end
 end
